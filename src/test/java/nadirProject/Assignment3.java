49d2d2d47150185d59d6c96ab709c5b680c4b986
Jjuthamas <ajuthamas.07@gmail.com>
2021-06-04 23:42:00 -0400

    second commit

--- src/test/java/nadirProject/Assignment3.java
+++ src/test/java/nadirProject/Assignment3.java
@@ -1,6 +1,9 @@
 package nadirProject;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -20,12 +23,25 @@ public class Assignment3 {
     }
     @AfterClass
     public void teardown(){
-        driver.close();
+        //driver.close();
     }
 
-    @Test
+    @Test (priority = 1)
     public void logIn(){
-
+        WebElement userName = driver.findElement(By.xpath("//input[@name='_username']"));
+        userName.sendKeys("user30");
+        WebElement passWord = driver.findElement(By.xpath("//input[@name='_password']"));
+        passWord.sendKeys("UserUser123", Keys.ENTER);
     }
+
+    @Test (priority = 2)
+    public void fleetSelector(){
+        WebElement fleetInfo = driver.findElement(By.xpath("//span[@class='title title-level-1']"));
+        fleetInfo.click();
+        WebElement vehicle = driver.findElement(By.xpath("//span[@class='title title-level-2']"));
+        vehicle.click();
+    }
+
+
 
 }
